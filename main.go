274770b55c15a4a18416d8142fd65a05c5499901
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-03-18 20:55:48 -0400

    added post and get features

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello-world", helloWorldFunc)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 func helloWorldFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request Received")
-	fmt.Fprintf(w, "Hello World Hola Mundo! '%v'", r.Method)
+	fmt.Fprintf(w, "Hello World Hola Mundo!")
 
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func main() {
+
+	fmt.Printf("Starting server at port 8080\n")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+
+	http.HandleFunc("/messages", helloWorldFunc)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
